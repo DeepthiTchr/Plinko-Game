2a706b63c63c75abc1a0760c2c497a2fb9f17281
KhushiAgarwal0406 <77060817+KhushiAgarwal0406@users.noreply.github.com>
2021-02-26 13:01:03 +0530

    Add files via upload

---
+++ division.js
@@ -0,0 +1,27 @@
+class Divisions{
+    constructor(x,y,width,height) {
+
+        var options = {
+
+            isStatic:true
+
+        }
+
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        
+        World.add(world,this.body);
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        rectMode(CENTER);
+        noStroke();
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+
+        
+    }
+}
